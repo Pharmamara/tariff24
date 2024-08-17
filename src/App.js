5f33756d11da807ea89945b5d302da7ff3ae164f
Pharmamara <140740925+Pharmamara@users.noreply.github.com>
2024-08-17 12:44:30 +0300

    24_1 useState add

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import React, { useState } from "react";
+import { TARIFFS } from "./components/tariffs.jsx";
 import Card from "./components/card";
 import "./App.css";
 
 function App() {
-  const [tariffs] = useState([
-    {
-      label: "tariff1",
-      style: { background: "#9ffceb" },
-      name: "Безлимитный 300",
-      price: "руб 300 /мес",
-      speed: "до 10 МБит/сек",
-      description: "объем включенного трафика не ограничен",
-    },
-    {
-      label: "tariff2",
-      style: { background: "#9ffca8" },
-      name: "Безлимитный 450",
-      price: "руб 400 /мес",
-      speed: "до 50 МБит/сек",
-      description: "объем включенного трафика не ограничен",
-    },
-    {
-      label: "tariff3",
-      style: { background: "#fc9fa5" },
-      name: "Безлимитный 550",
-      price: "руб 550 /мес",
-      speed: "до 100 МБит/сек",
-      description: "объем включенного трафика не ограничен",
-    },
-    {
-      label: "tariff4",
-      style: { background: "#666" },
-      name: "Безлимитный 1000",
-      price: "руб 1000 /мес",
-      speed: "до 200 МБит/сек",
-      description: "объем включенного трафика не ограничен",
-    },
-  ]);
+  const [tariffs, setTariff] = useState(TARIFFS);
 
-  /*const handleChange = (index) => {
+  const handleChange = (index) => {
     const selectedTariffs = [...tariffs];
+    selectedTariffs[index].className = "cardActive";
     selectedTariffs[index].name = `выбран тариф ${tariffs[index].name}`;
     selectedTariffs[index].price = `за ${tariffs[index].price}`;
     selectedTariffs[index].speed = `скорость ${tariffs[index].speed}`;
     selectedTariffs[index].description = `${tariffs[index].description}`;
     setTariff(selectedTariffs);
-  };*/
-
-  const handleClick = (event) => {
-    event.currentTarget.style.animation = "myAnim 2s ease 0s 1 normal none";
   };
+
   return (
     <div
       style={{
@@ -62,23 +28,13 @@ function App() {
       {tariffs.map((Tariffs, index) => {
         return (
           <div
-            style={{
-              width: "25vw",
-              height: "25vw",
-              display: "flex",
-              flexDirection: "column",
-              textAlign: "center",
-              animation: "box-shadow 3s ease",
-              cursor: "pointer",
-            }}
-            onClick={handleClick}
-            //объединить изменение стилей и данных в элементе
-            /*onClick={() => {
+            onClick={() => {
               handleChange(index);
             }}
-            key={index}*/
+            key={index}
           >
             <Card
+              className="cardActive"
               style={Tariffs.style}
               key={index}
               name={Tariffs.name}
